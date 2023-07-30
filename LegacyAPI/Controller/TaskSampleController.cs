2de70747b27df53d1a108c255c925cffe97a50e4
Victoria Stoica <Victoria.Stoica@endava.com>
2023-07-30 11:34:55 +0300

    Add project files.

---
+++ LegacyAPI/Controller/TaskSampleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LegacyAPI.Controller
+{
+    public class TaskSampleController : BaseController
+    {
+        [HttpPut]
+        [Route("api/init/{id}")]
+        public string Init(int taskId)
+        {
+            return "OK";
+        }
+
+        [HttpPut]
+        [Route("api/confirm/{id}")]
+        public string Confirm(int taskId)
+        {
+            return "OK";
+        }
+
+        [HttpPut]
+        [Route("api/reject/{id}")]
+        public string Reject(int taskId)
+        {
+            return "OK";
+        }
+
+        [HttpPut]
+        [Route("api/update/{id}")]
+        public string Update(int taskId)
+        {
+            return "OK";
+        }
+    }
+}
---
+++ LegacyAPI/Model/TaskSample.cs
@@ -0,0 +1,13 @@
+﻿namespace LegacyAPI.Model
+{
+    public class TaskSample
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; } = string.Empty;
+        public DateTime TaskDate { get; set; }
+        public string TaskType { get; set; } = string.Empty;
+        public bool TaskIsProcessed { get; set; }
+        public DateTime TaskModifyDate { get; set; }
+        public string TaskModifyBy { get; set; } = string.Empty;
+    }
+}
